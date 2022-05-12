ff40a97d9c2f11b7a8ebf60613a3ea5b6983cb8a
TropicolX <jacobsbusayo@gmail.com>
2022-05-12 14:54:41 +0100

    checkbox testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
 	const [buttonColor, setButtonColor] = useState("red");
+	const [disabled, setDisabled] = useState(false);
 
 	const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
@@ -11,13 +12,29 @@ function App() {
 	};
 
 	return (
-		<div>
+		<div
+			style={{
+				height: "100vh",
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
 			<button
 				onClick={changeButtonColor}
 				style={{ backgroundColor: buttonColor }}
+				disabled={disabled}
 			>
 				Change to {newButtonColor}
 			</button>
+			<input
+				type="checkbox"
+				id="disable-button-checkbox"
+				defaultChecked={disabled}
+				aria-checked={disabled}
+				onChange={(e) => setDisabled(e.target.checked)}
+			/>
 		</div>
 	);
 }
